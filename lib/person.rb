84f88e3a89bb685b3f7cb7a87f70a929d435d0b0
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-10-06 20:51:30 -0400

    object attributes

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+    def name
+        @name
+    end
+
+    def name=(pref_name)
+        @name = pref_name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(new_job)
+        @job = new_job
+    end
+    
+    
+end
